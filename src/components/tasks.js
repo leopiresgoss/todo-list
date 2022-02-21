eddb57bcdaddbb87200f179ab06fd3cbfbc2e1c4
Fabio Coelho <46031174+fbclh@users.noreply.github.com>
2022-02-21 12:41:36 -0300

    Revert "To Do list: interactive list"

--- src/components/tasks.js
+++
@@ -1,9 +0,0 @@
-class Tasks {
-  constructor(description = 'All clean', completed = false, index = 0) {
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
-  }
-}
-
-export default Tasks;
